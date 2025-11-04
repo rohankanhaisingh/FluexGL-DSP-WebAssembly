97573beb37d44a9b7159ccc526a560e38fcc908e
Rohan Kanhaisingh <tr.kanhaisingh@gmail.com>
2025-11-04 12:26:48 +0100

    AudioWorkletProcessor scripts should be created within this project in order to glue the neccesary code together. Results will eventually be used as modular input for the DSP pipeline.

---
+++ worklets/src/SoftClipProcessor.ts
@@ -0,0 +1,66 @@
+import { SoftClip, initSync } from "../../_dist/wasm/fluexgl-dsp-wasm";
+import { AudioWorkletProcessor, type AudioWorkletNodeOptions } from "./typings";
+
+class SoftClipProcessor extends AudioWorkletProcessor {
+
+    public softClip: SoftClip | null = null;
+    public drive: number = 0;
+
+    constructor(options: AudioWorkletNodeOptions) {
+        super();
+
+        if (!options.processorOptions?.module) throw new Error("Could not construct AudioWorkletProcessor instance, because the required WASM module has not been provided.");
+
+        initSync({ module: options.processorOptions?.module })
+
+        this.softClip = new SoftClip(options.parameterData?.drive ?? 0)
+    }
+
+    public process(inputs: Float32Array[][], outputs: Float32Array[][], parameters: any): boolean {
+
+        if (!this.softClip) return false;
+
+        const input = inputs[0];
+        const output = outputs[0];
+
+        if(!input || !output) return false;
+
+        if (!input || input.length === 0) {
+            
+            for (let ch = 0; ch < output.length; ch++) {
+                output[ch]?.fill(0);
+            }
+
+            return true;
+        }
+
+        if (!this.isReady || !this.softClip) {
+            for (let ch = 0; ch < input.length; ch++) {
+                if (!input[ch] || !output[ch]) 
+                    continue;
+
+                output[ch]?.set(input[ch] ?? []);
+            }
+
+            return true;
+        }
+
+        const driveParameter: number = parameters.drive ?? 0;
+
+        this.softClip.set_drive(driveParameter);
+
+        for (let ch = 0; ch < input.length; ch++) {
+            const inChan = input[ch];
+            const outChan = output[ch];
+
+            if (!inChan || !outChan) continue;
+
+            outChan.set(inChan);
+            this.softClip.process(outChan);
+        }
+
+        return true;
+    }
+}
+
+registerProcessor("SoftClipProcessor", SoftClipProcessor)
---
+++ worklets/src/typings.ts
@@ -0,0 +1,46 @@
+declare abstract class AudioWorkletProcessor {
+
+    public readonly port: MessagePort;
+    public static parameterDescriptors?: AudioParamDescriptor[];
+
+    protected isReady: boolean;
+    protected module: WebAssembly.Module | null;
+
+    constructor(options?: AudioWorkletNodeOptions);
+
+    public process(inputs: Float32Array[][], outputs: Float32Array[][], parameters: any): boolean;
+}
+
+interface AudioWorkletNodeOptions extends AudioNodeOptions {
+    numberOfInputs?: number;
+    numberOfOutputs?: number;
+    outputChannelCount?: number[];
+    parameterData?: Record<string, number>;
+    processorOptions?: {
+        module: WebAssembly.Module
+    };
+}
+
+interface AudioParamDescriptor {
+    name: string;
+    defaultValue?: number;
+    minValue?: number;
+    maxValue?: number;
+    automationRate?: "k-rate" | "a-rate";
+}
+
+interface AudioWorkletProcessorConstructor {
+    new (...args: any[]): AudioWorkletProcessor;
+    parameterDescriptors?: AudioParamDescriptor[];
+}
+
+declare global {
+    function registerProcessor(name: string, processorCtor: AudioWorkletProcessorConstructor): void;
+}
+
+export {
+    AudioWorkletProcessor,
+    type AudioParamDescriptor,
+    type AudioWorkletProcessorConstructor,
+    type AudioWorkletNodeOptions
+}
